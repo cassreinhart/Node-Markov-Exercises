32d50a792ed33e1dadcc9f229da20edd8711e80b
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-01-12 15:21:31 -0600

    first draft markov generator

--- markov.js
+++ markov.js
@@ -17,13 +17,33 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let chains = {};
+
+    for (let word of words) {
+      if (!Object.keys(chains).indexOf(word)) {
+        chains[word] = Array.from(words[words.indexOf(word) + 1]);
+      } else {
+        chains[word].push(words[words.indexOf(word) + 1])
+      }
+    }
+    return chains;
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let text = '';
+    for (let i = 1; i <= numWords; i++) {
+      
+      if (text.length > 0) {
+        let randomWordIdx = Math.floor(Math.random() * chains.length + 1)
+        text+= Object.keys(chains)[randomWordIdx];
+      } else {
+        let randomChainIdx = Math.floor(Math.random() * chains[i].length + 1) //this won't work
+        text+= chains[Object.keys(chains)]
+      }
+    }
+    return text;
   }
 }
