dce761055201ce17733314e982aebc3d9f4e10b2
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-01-17 12:36:49 -0600

    added makeText

--- makeText.js
+++ makeText.js
@@ -1,2 +1,37 @@
 /** Command-line tool to generate Markov text. */
 
+import fs from 'fs';
+import axios from 'axios';
+const markov = require('./markov');
+import process from 'process';
+
+
+function generateText(text) {
+    let mm = new markov.MarkovMachine(text);
+    console.log(mm.makeText());
+}
+
+function makeText(path) {
+    fs.readFile(path, "utf8", function cb(err, data) {
+      if (err) {
+        console.error(`Cannot read file: ${path}: ${err}`);
+        process.exit(1);
+      } else {
+        generateText(data);
+      }
+    });
+  
+} 
+
+async function makeURLText(url) {
+    let resp;
+  
+    try {
+      resp = await axios.get(url);
+    } catch (err) {
+      console.error(`Cannot read URL: ${url}: ${err}`);
+      process.exit(1);
+    }
+    generateText(resp.data)
+}
+  
